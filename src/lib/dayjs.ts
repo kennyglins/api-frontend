1789fb30986f18fa5bc19d576efe9e40a1ab0945
kennyglins <nino010506@gmail.com>
2025-10-30 19:47:48 -0300

    fix: correct dayjs locale path (pt-br lowercase)

--- src/lib/dayjs.ts
+++ src/lib/dayjs.ts
@@ -2,7 +2,7 @@ import lib from "dayjs"
 import "dayjs/locale/pt-BR"
 import relativeTime from "dayjs/plugin/relativeTime"
 
-lib.locale("pt-BR")
+lib.locale("pt-br")
 lib.extend(relativeTime)
 
 export const dayjs = lib
